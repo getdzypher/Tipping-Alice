4a6a49cdda4b987abe2c18d6c9c7635bf2ebfd18
dzypher <65742350+getdzypher@users.noreply.github.com>
2020-07-10 18:46:16 +0800

    Update Keyboard.java

--- src/main/java/info/bcdev/telegramwallet/bot/Keyboard.java
+++ src/main/java/info/bcdev/telegramwallet/bot/Keyboard.java
@@ -1,4 +1,4 @@
-package info.bcdev.telegramwallet.bot;
+package info.bcdev.alicewallet.bot;
 
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
