e8d0163d6a32c736b70d20bc6cb30426b9275725
dzypher <65742350+getdzypher@users.noreply.github.com>
2020-07-10 18:46:47 +0800

    Update PageCreateWallet.java

--- src/main/java/info/bcdev/telegramwallet/ethereum/pages/PageCreateWallet.java
+++ src/main/java/info/bcdev/telegramwallet/ethereum/pages/PageCreateWallet.java
@@ -1,14 +1,14 @@
-package info.bcdev.telegramwallet.ethereum.pages;
+package info.bcdev.alicewallet.ethereum.pages;
 
 import com.vdurmont.emoji.EmojiParser;
-import info.bcdev.telegramwallet.Main;
-import info.bcdev.telegramwallet.Settings;
-import info.bcdev.telegramwallet.bot.BotInstance;
-import info.bcdev.telegramwallet.bot.KeyBoards;
-import info.bcdev.telegramwallet.bot.Keyboard;
-import info.bcdev.telegramwallet.bot.Tbot;
-import info.bcdev.telegramwallet.bot.session.Session;
-import info.bcdev.telegramwallet.ethereum.CreateEW;
+import info.bcdev.alicewallet.Main;
+import info.bcdev.alicewallet.Settings;
+import info.bcdev.alicewallet.bot.BotInstance;
+import info.bcdev.alicewallet.bot.KeyBoards;
+import info.bcdev.alicewallet.bot.Keyboard;
+import info.bcdev.alicewallet.bot.Tbot;
+import info.bcdev.alicewallet.bot.session.Session;
+import info.bcdev.alicewallet.ethereum.CreateEW;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static info.bcdev.telegramwallet.bot.session.Session.SETTINGS;
+import static info.bcdev.alicewallet.bot.session.Session.SETTINGS;
 
 public class PageCreateWallet implements KeyBoards, BotInstance {
 
